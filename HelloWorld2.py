785c4252efab9afd25e41c65eb9ea285cadab627
Jason Eckert <jasoneckert4@gmail.com>
2023-11-02 15:54:29 +0000

    created

--- HelloWorld2.py
+++ HelloWorld2.py
@@ -1,1 +1,9 @@
-print("hello world!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def Hello():
+    return 'Hello, earth!'
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True, host="0.0.0.0")
